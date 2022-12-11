4715de2a0b6bcf50103645865e253566325f0436
AnimMouse <git@animmouse.com>
2022-12-11 17:51:56 +0800

    Enable ngrams

--- languagetool.sh
+++ languagetool.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Xms256m -Xmx512m -cp languagetool-server.jar org.languagetool.server.HTTPServer --port 10000 --public --allow-origin '*'
+java -Xms256m -Xmx512m -cp languagetool-server.jar org.languagetool.server.HTTPServer --port 10000 --public --allow-origin '*' --languageModel /ngrams/
