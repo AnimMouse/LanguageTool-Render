f1653a6e75314613e9619370c2b12abfc60fd985
AnimMouse <git@animmouse.com>
2022-12-12 12:26:01 +0800

    Use other folder for ngrams

--- languagetool.sh
+++ languagetool.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-mkdir /tmp/ngrams/ /tmp/ngrams-tmp/
-(wget https://languagetool.org/download/ngram-data/ngrams-en-20150817.zip -P /tmp/ && cd /tmp/ngrams-tmp/ && jar -xvf /tmp/ngrams-en-20150817.zip && mv /tmp/ngrams-tmp/* /tmp/ngrams/ && rm -r /tmp/ngrams-en-20150817.zip /tmp/ngrams-tmp/ && echo Ngram has been set up) &
-java -Xms256m -Xmx512m -cp languagetool-server.jar org.languagetool.server.HTTPServer --port 10000 --public --allow-origin '*' --languageModel /tmp/ngrams/
+(wget https://languagetool.org/download/ngram-data/ngrams-en-20150817.zip -P /ngrams-download/ && cd /ngrams-tmp/ && jar -xvf /ngrams-download/ngrams-en-20150817.zip && mv /ngrams-tmp/* /ngrams/ && rm /ngrams-download/ngrams-en-20150817.zip && echo Ngram has been set up) &
+java -Xms256m -Xmx512m -cp languagetool-server.jar org.languagetool.server.HTTPServer --port 10000 --public --allow-origin '*' --languageModel /ngrams/
